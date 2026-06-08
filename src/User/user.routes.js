8802d2159dab45897cc111d88385560665d42d9f
Compkev22 <kvelasquez-2022473@kinal.edu.gt>
2026-06-08 10:22:06 -0600

    Fix 4: Proteger con JWT la ruta de la entidad Usuarios

--- src/User/user.routes.js
+++ src/User/user.routes.js
@@ -1,26 +1,19 @@
 import { Router } from 'express';
-import {
-    updateUser,
-    getProfile,
-} from './user.controller.js';
-
-import {
-    validateUpdateUserRequest,
-} from '../../middlewares/user-validator.js';
-
+import { updateUser, getProfile } from './user.controller.js';
+import { validateUpdateUserRequest } from '../../middlewares/user-validator.js';
 import { validateJWT } from '../../middlewares/validate-jwt.js';
+import { hasRole } from '../../middlewares/role-validator.js';
 
 const router = Router();
 
-
-router.get('/profile', getProfile);
+router.get('/profile', validateJWT, hasRole('CLIENT'), getProfile);
 
 router.put(
     '/:id',
     validateJWT,
+    hasRole('CLIENT'),
     validateUpdateUserRequest,
     updateUser
 );
-
 
 export default router;
